906dc1609eb32d269773ce393c0325663ba1691c
Jordan Corley <rainwolfj@gmail.com>
2016-03-08 16:47:45 -0600

    Added part 2, currently only builds array and spits it out for inspection

---
+++ part2.rb
@@ -0,0 +1,29 @@
+movies = []
+
+    movies << {
+      title: "Forest Gump",
+      budget: 55,
+      stars: ["Tom Hanks"]
+    }
+    movies << {
+      title: "Star Wars",
+      budget: 11,
+      stars: ["Mark Hamill", "Harrison Ford"]
+    }
+    movies << {
+      title: "Batman Begins",
+      budget: 150,
+      stars: ["Christian Bale", "Liam Neeson", "Michael Caine"]
+    }
+    movies << {
+      title: "Titanic",
+      budget: 200,
+      stars: ["Kate Winslet", "Leonardo DiCaprio"]
+    }
+    movies << {
+      title: "Inception",
+      budget: 160,
+      stars: ["Leonardo DiCaprio", "JGL"]
+    }
+
+puts movies
